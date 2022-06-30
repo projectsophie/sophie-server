6c744476ea6b3edd0fe130a9954bb5d9849ee08f
DiOnFire <di0nik.gd@gmail.com>
2022-07-01 00:41:22 +0300

    Additional validation

--- store/user.go
+++ store/user.go
@@ -11,6 +11,9 @@ import (
 
 // CreateUser Creates user instance and adds it to database
 func CreateUser(model model.UserCreate) gin.H {
+	if len(model.Firstname) < 2 || len(model.Lastname) < 2 || len(model.Password) < 8 || len(model.Nickname) < 3 {
+		return gin.H{"error": "invalid data"}
+	}
 	statement, _ := database.GetUsersDB().Prepare(database.CREATE_USER)
 	password := util.GenerateHash([]byte(model.Password))
 	_, err := statement.Exec(model.Firstname, model.Lastname, model.Nickname, password, model.Email)
